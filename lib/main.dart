54df1723377525cc8991d2b39eab6896f54347a4
Vishnu Haridas <hvishnu999@gmail.com>
2020-03-04 00:16:37 +0530

    Add app theme for Dark mode.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        canvasColor: Colors.black,
+        primaryColor: Color(0xFF8563EA),
+        accentColor: Colors.white,
       ),
       home: LoginScreen(),
     );
