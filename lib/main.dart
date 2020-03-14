d76c08341c8a5a9c197ee802957453f16dc96f07
Vishnu Haridas <hvishnu999@gmail.com>
2020-03-14 21:47:25 +0530

    Add `AssignmentDataProvider` and use in the MainApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:TimeTek/provider/assignment_data.dart';
 import 'package:TimeTek/ui/home/home_screen.dart';
-import 'package:TimeTek/ui/signup_screen.dart';
 import 'package:TimeTek/ui/login_screen.dart';
+import 'package:TimeTek/ui/signup_screen.dart';
 import 'package:TimeTek/util/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,22 +12,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TimeTek',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        canvasColor: Colors.black,
-        primaryColor: Color(0xFF8563EA),
-        accentColor: Colors.white,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AssignmentDataProvider(),),
+        // space to add more providers later.
+      ],
+      child: MaterialApp(
+        title: 'TimeTek',
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          canvasColor: Colors.black,
+          primaryColor: Color(0xFF8563EA),
+          accentColor: Colors.white,
+        ),
+
+        initialRoute: ROUTE_LOGIN,
+        routes: {
+          ROUTE_LOGIN: (context) => LoginScreen(),
+          ROUTE_SIGN_UP: (context) => SignUpScreen(),
+          ROUTE_HOME: (context) => HomeScreen(),
+        },
+
       ),
-
-      initialRoute: ROUTE_LOGIN,
-      routes: {
-        ROUTE_LOGIN: (context) => LoginScreen(),
-        ROUTE_SIGN_UP: (context) => SignUpScreen(),
-        ROUTE_HOME: (context) => HomeScreen(),
-      },
-
     );
   }
 }
