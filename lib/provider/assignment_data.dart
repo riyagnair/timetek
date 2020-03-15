384dab3c7ff557215c35ad71987a9077e2b479c7
Vishnu Haridas <hvishnu999@gmail.com>
2020-03-16 00:54:10 +0530

    AssignmentDataProvider: add weekly slots info.

--- lib/provider/assignment_data.dart
+++ lib/provider/assignment_data.dart
@@ -1,7 +1,29 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AssignmentDataProvider extends ChangeNotifier {
 
+  List<int> weeklySlots = [1,1,1,1,1,1,1];
+
+  void setWeeklySlot(int weekDay, int minutes){
+    weeklySlots[weekDay] = minutes;
+    notifyListeners();
+  }
+
+  Future<List<int>> loadWeeklySlots() async {
+    var prefs = await SharedPreferences.getInstance();
+    var slotsJson = prefs.getString("weekly_slots") ?? '''[1,1,1,1,1,1,1]''';
+    weeklySlots = (jsonDecode(slotsJson) as List).map((e) => e as int).toList();
+    return weeklySlots;
+  }
+
+  Future<bool> saveWeeklySlots(List<int> slots) async {
+    var prefs = await SharedPreferences.getInstance();
+    debugPrint(jsonEncode(slots));
+    prefs.setString("weekly_slots", jsonEncode(slots));
+  }
 
 
 }
