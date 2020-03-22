777899cfcfcdc6e1cbab88e7c2841e9302a1ee7d
Vishnu Haridas <hvishnu999@gmail.com>
2020-03-22 22:43:37 +0530

    History screen: show empty text when there is no data.

--- lib/ui/history_screen.dart
+++ lib/ui/history_screen.dart
@@ -5,35 +5,35 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
-class HistoryScreen extends StatelessWidget{
+class HistoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Selector<AssignmentDataProvider, List<Assignment>>(
-      selector: (ctx , provider ) => provider.assignments,
+      selector: (ctx, provider) => provider.assignments,
       builder: (BuildContext context, List<Assignment> list, Widget child) {
-        return ListView.builder(
-          itemCount: list.length,
-          itemBuilder: (BuildContext context, int index){
-            return ListTile(
-              onTap: (){},
-              title: Text(
-                list[index].title,
-                style: GoogleFonts.raleway(
-                  color: Colors.white
-                )
-              ),
-              subtitle: Text(
-                "Due on ${DateFormat("yyyy-MM-dd").format(list[index].startDate)}",
-                style: GoogleFonts.raleway(
-                  color: Colors.grey
-                )
-              ),
-              leading: Icon(Icons.today),
-            );
-          }
-        );
+        if (list == null || list.isEmpty) {
+
+          // A text showing that there's no data available yet.
+          return Center(
+            child: Text("You don't have any assignments yet."),
+          );
+        } else {
+
+          // List of items.
+          return ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                onTap: () {},
+                title: Text(list[index].title, style: GoogleFonts.raleway(color: Colors.white)),
+                subtitle: Text("Due on ${DateFormat("yyyy-MM-dd").format(list[index].startDate)}",
+                    style: GoogleFonts.raleway(color: Colors.grey)),
+                leading: Icon(Icons.today),
+              );
+            },
+          );
+        }
       },
     );
   }
-
 }
