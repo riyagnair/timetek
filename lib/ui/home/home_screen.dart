8f4f01bdc94cdaada51e123272ab9c7a041ac4ea
Vishnu Haridas <hvishnu999@gmail.com>
2020-03-22 23:10:57 +0530

    Move history screen from `ui.` to `ui.history.` directory

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:TimeTek/provider/assignment_data.dart';
 import 'package:TimeTek/provider/user_data.dart';
 import 'package:TimeTek/ui/account/my_account_screen.dart';
-import 'package:TimeTek/ui/history_screen.dart';
+import 'package:TimeTek/ui/history/history_screen.dart';
 import 'package:TimeTek/util/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
